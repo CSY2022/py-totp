73bd4e7e8a2417923dfc27e760c32304a4b3235a
CSY2022 <tel19807034568@outlook.com>
2024-01-29 17:33:04 +0800

    Add files via upload

--- src/source.py
+++ src/source.py
@@ -6,14 +6,28 @@ import hmac
 import hashlib
 import struct
 import base64
+import PySimpleGUI as sg
+import pyperclip as cb
 
+def init():
+    secret_key = sg.popup_get_text('输入你的密钥：', title="初始化") 
+    secret_key.replace(' ','')
+    secret_key.replace('\n','')
+    secret_key.replace('\r','')
+    f=open('./key.txt' ,'x')
+    f.close()
+    f=open('./key.txt' ,'w')
+    f.write(secret_key)
+    f.close()
+    return secret_key
 def ntp():
     try:
       c = ntplib.NTPClient()
-      response = c.request('http://cn.ntp.org.cn')
+      response = c.request('http://cn.pool.ntp.org')
       return response.tx_time
     except:
-      return None  
+      return None
+    
 def generate_otp(secret,digits=6, hash_func=hashlib.sha1):
     time_step_count = current_time // 30
     time_step_bytes = struct.pack(">Q", time_step_count)
@@ -24,7 +38,25 @@ def generate_otp(secret,digits=6, hash_func=hashlib.sha1):
     otp_value = otp_value & 0x7FFFFFFF
     otp_value = str(otp_value % (10 ** digits)).zfill(digits)
     return otp_value
-while True:  
+sg.set_options(font=('Arial Bold', 18))
+sg.theme('DarkAmber')
+try:
+    f=open('./key.txt' ,'r')
+    secret_key=f.readline()
+    f.close()
+except:
+    secret_key=init()
+if secret_key==None:
+    secret_key=init()
+layout = [
+   [sg.Text('TOTP:'), sg.Text(key='-TOTP-')],
+   [sg.Text('有效期:'), sg.Text(key='-TIME-')],
+   [sg.Button(" 复制 TOTP ")],
+]
+window=sg.Window("TOTP",layout)
+while True:
+  if secret_key == None:
+      break
   global current_time
   ntp_time = ntp()
   local_time = time()
@@ -34,15 +66,14 @@ while True:
   else:
       current_time=int(local_time)
       delay_time=0
-  secret_key = " Your secret key here"
   otp = generate_otp(secret_key)
-  system('cls')
-  print("生成的TOTP:", otp)
-  print("有效期:",int(30-(current_time % 30)+0.5),"s")
   stop_time=30-(current_time % 30)
   while 30-(current_time % 30)<=stop_time and 30-(current_time % 30)>=0.5:
     current_time=delay_time+time()
-    system('cls')
-    print("生成的TOTP:", otp)
-    print("有效期:",int(30-(current_time % 30)+0.5),"s")
-    sleep(0.5)  
+    event, values = window.read(timeout=0)
+    window['-TOTP-'].update(str(otp))
+    window['-TIME-'].update(str(int(30-(current_time % 30)+1))+'s')
+    if event==" 复制 TOTP ":
+        cb.copy(str(otp))
+        window.close()
+    sleep(0.5)
